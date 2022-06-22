e31f428e70fa9a875bb3d60f736801cf6ab77622
jjy12332 <jia_de163@163.com>
2022-06-22 20:06:53 +0800

    解决数据库连接问题，可以开发业务相关代码

--- src/main/java/com/app/bean/userHealth.java
+++ src/main/java/com/app/bean/userHealth.java
@@ -18,8 +18,6 @@ public class userHealth {
     private Date CreateTime;
     //地址
     private String NowAddress;
-    //是否请假(1是，0否),数据库这个字段默认给0，如有写入自动改为1，且用户请假则会一次性写入数据库请假天数的条数数据，此数据会变为1
-    //private String LeaveFlag;
     //用户id(唯一键open_id)
     private String OpenId;
 }
--- src/main/java/com/app/mapper/userHealthMapper.java
+++ src/main/java/com/app/mapper/userHealthMapper.java
@@ -3,9 +3,11 @@ package com.app.mapper;
 
 import com.app.bean.userHealth;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 //如果不加下面注解会报错，找不到此文件
 @Mapper
+@Repository
 public interface userHealthMapper {
      userHealth GetName(String user_name);
      void InsertData(userHealth e);
